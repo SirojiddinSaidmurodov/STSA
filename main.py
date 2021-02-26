e24e4990e098fca5275d17ec2fdec617dad9cef5
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-02-26 11:42:01 +0300

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from scipy.optimize import linprog
+import numpy as np
+import time
+start = time.time()
+c = np.genfromtxt("c.csv", delimiter=",", usemask=True)
+A_ub = np.genfromtxt("aub.csv", delimiter=",", usemask=True)
+b_ub = np.genfromtxt("bub.csv", delimiter=",", usemask=True)
+A_eq = np.genfromtxt("aeq.csv", delimiter=",", usemask=True)
+b_eq = np.genfromtxt("beq.csv", delimiter=",", usemask=True)
+print(linprog(c, A_ub, b_ub, A_eq, b_eq))
+stop = time.time()
+print ("Время :")
+print(stop - start)
