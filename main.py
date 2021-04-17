735f6eeaa6ef8c16978536617c98f26ed3b3adda
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-04-18 02:16:06 +0300

    add first phase(seems ok)
    Took 2 hours 12 minutes

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-import simplex
+import simplexsolver
 
 if __name__ == '__main__':
-    sim = simplex.Simplex(func=np.array([1, 2], dtype=float),
-                          bounds_matrix=np.array([[7, 3, -1, 0, 0, 0],
-                                                  [2, 7, 0, -1, 0, 0],
-                                                  [6, 4, 0, 0, -1, 0],
-                                                  [8, 2, 0, 0, 0, -1]], dtype=float),
-                          bounds_vector=np.array([13, 11, 10, 13], dtype=float))
+    sim = simplexsolver.SimplexSolver(func=np.array([1, 2], dtype=float),
+                                      bounds_matrix=np.array([[7, 3, -1, 0, 0, 0],
+                                                              [2, 7, 0, -1, 0, 0],
+                                                              [6, 4, 0, 0, -1, 0],
+                                                              [8, 2, 0, 0, 0, -1]], dtype=float),
+                                      bounds_vector=np.array([13, 11, 10, 13], dtype=float))
     print(sim)
     sim.solve(debug=True)
---
+++ simplexsolver.py
@@ -0,0 +1,110 @@
+import logging
+
+import numpy as np
+
+
+class SimplexSolver:
+    __doc__ = "My own realization of simplex algorithm." \
+              "Just use solve method"
+
+    def __init__(self,
+                 func: np.ndarray,
+                 bounds_matrix: np.ndarray,
+                 bounds_vector: np.ndarray):
+        """
+        Initializing the solver
+        :param func : The coefficients of the linear objective function to be minimized.
+
+        :param bounds_matrix : The equality constraint matrix. Each row of ``bounds_matrix`` specifies the
+        coefficients of a linear equality constraint on ``x``.
+
+        :param bounds_vector : The equality constraint vector. Each element of ``bounds_matrix @ x`` must equal
+        the corresponding element of ``bounds_vector``.
+        """
+        np.set_printoptions(precision=4, suppress=True, threshold=np.inf)
+        self.func, self.bounds_matrix, self.bounds_vector = func.copy(), bounds_matrix.copy(), bounds_vector.copy()
+        self.simplex_bound_coefficients: np.ndarray
+        self.obj_function_coefficients: np.ndarray
+        self.c_basis: np.ndarray
+
+    def solve(self, debug=False):
+        """Solve method
+        :param debug True for logging level DEBUG, level INFO otherwise
+        """
+        logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+        self.__init_simplex_matrix()
+        self.__first_phase_solve()
+
+    def __init_simplex_matrix(self):
+        n, m = self.bounds_matrix.shape
+        self.simplex_bound_coefficients = np.hstack(
+            (np.column_stack((self.bounds_vector.transpose(), self.bounds_matrix)),
+             np.eye(n)))
+        self.c_basis = np.array(range(m + 1, m + n + 1))
+
+        logging.debug('=============================================\n'
+                      'artificial variables added:\n' + str(self.simplex_bound_coefficients)
+                      + '\nbasis:\n' + str(self.c_basis) + '\n\n')
+
+    def __first_phase_solve(self):
+        n, m = self.bounds_matrix.shape
+        self.obj_function_coefficients = np.hstack((np.zeros((m + 1,)), np.ones(n, )))  # m + 1 for calculating delta_0
+        logging.debug('=============================================\n'
+                      'current coefficients of objective function:\n' +
+                      str(self.obj_function_coefficients) + '\n\n')
+        basis_track = [i for i in range(n)]  # start indexes
+        for i in range(n):
+            rate: np.ndarray = self.__calc_rate()
+            rate = rate[1:]
+            new_var_column = rate.tolist().index(max(rate)) + 1
+            new_var_row = self.__find_row_index(p_0=self.simplex_bound_coefficients[:, 0],
+                                                p_n=self.simplex_bound_coefficients[:, new_var_column],
+                                                track=basis_track)
+            logging.debug('New basis row' + str(new_var_row))
+
+            self.simplex_bound_coefficients[new_var_row, :] = \
+                self.simplex_bound_coefficients[new_var_row, :] / \
+                self.simplex_bound_coefficients[new_var_row, new_var_column]
+
+            self.c_basis[new_var_row] = self.obj_function_coefficients[new_var_column]
+            self.__calc_c_basis(new_var_row, new_var_column)
+            logging.debug('=====================================\n'
+                          'current coefficients of objective function:\n' + str(self.obj_function_coefficients) + '\n')
+            logging.debug('=====================================\n'
+                          'current basis vars:\n' + str(self.simplex_bound_coefficients) + '\n\n')
+
+    def __find_row_index(self, p_0, p_n, track):
+        if (p_n > 0).sum() > 0:
+            candidate_indexes: np.ndarray = p_0 / p_n
+            can = candidate_indexes.copy()
+            can.sort()
+            for i in can:
+                if i > 0 and candidate_indexes.tolist().index(i) in track:
+                    return candidate_indexes.tolist().index(i)
+        else:
+            raise Exception('There is no solution')
+
+    def __str__(self):
+        return '\n\tfunc:\n' + str(self.func) + \
+               '\n\n\tbounds_matrix:\n' + str(self.bounds_matrix) + \
+               '\n\n\tbounds_vector:\n' + str(self.bounds_vector)
+
+    def __calc_rate(self):
+        coefficients = []
+        for i in self.c_basis:
+            coefficients.append(self.obj_function_coefficients[i])
+        coefficients = np.array(coefficients).transpose()
+
+        rate = (np.dot(coefficients, self.simplex_bound_coefficients)) - self.obj_function_coefficients
+        logging.debug('==============================================\n'
+                      'rates:\n' + str(rate) + '\n\n')
+        return rate
+
+    def __calc_c_basis(self, row, column):
+        m, n = self.simplex_bound_coefficients.shape
+        for i in range(m):
+            if i != row:
+                self.simplex_bound_coefficients[i, :] = np.subtract(
+                    self.simplex_bound_coefficients[i, :],
+                    self.simplex_bound_coefficients[i, column].item() *
+                    self.simplex_bound_coefficients[row, :])
