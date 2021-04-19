3d398366e8e2c089cd20bb9412cf235a241f964f
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-04-19 21:29:14 +0300

    add comparing linprog vs my simplex
    Took 30 seconds

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.optimize as sc
 
 import simplexsolver
 
@@ -8,6 +9,15 @@ if __name__ == '__main__':
                                                               [2, 7, 0, -1, 0, 0],
                                                               [6, 4, 0, 0, -1, 0],
                                                               [8, 2, 0, 0, 0, -1]], dtype=float),
-                                      bounds_vector=np.array([13, 11, 10, 13], dtype=float))
+                                      bounds_vector=np.array([13, 11, 10, 13], dtype=float),
+                                      precision=7)
     print(sim)
     sim.solve(debug=True)
+
+    opt = sc.linprog(c=np.array([1, 2, 0, 0, 0, 0], dtype=float),
+                     A_eq=np.array([[7, 3, -1, 0, 0, 0],
+                                    [2, 7, 0, -1, 0, 0],
+                                    [6, 4, 0, 0, -1, 0],
+                                    [8, 2, 0, 0, 0, -1]], dtype=float),
+                     b_eq=np.array([13, 11, 10, 13], dtype=float))
+    print(opt)
