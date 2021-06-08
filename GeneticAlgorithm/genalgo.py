84b9ace0d49c2feebbf776fb1a4187ed5e175eed
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-06-08 03:32:26 +0300

    refactor: moved fitness function computing to the Phenotype
    Took 14 minutes

--- GeneticAlgorithm/genalgo.py
+++ GeneticAlgorithm/genalgo.py
@@ -1,13 +1,14 @@
 import random
 from inspect import signature
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 
 class Phenotype:
-    def __init__(self, genotype: list):
+    def __init__(self, genotype: list, fitness_function: callable):
         self.genotype = genotype
+        self.fitness_function = fitness_function
+
+    def fitness(self):
+        return self.fitness_function(*self.genotype)
 
     def __repr__(self):
         return str(self.genotype)
@@ -21,34 +22,31 @@ class GenAlgo:
         self.POPULATION_SIZE = population_size
         self.MAX_GENERATIONS = max_generations
         self.PARAM_SIZE = len(signature(self.fitness).parameters)
-        self.population = [Phenotype([random.randint(int(bounds[0]), int(bounds[1])) for _ in range(self.PARAM_SIZE)])
-                           for _ in range(self.POPULATION_SIZE)]
+        self.population = [
+            Phenotype([random.randint(int(bounds[0]), int(bounds[1])) for _ in range(self.PARAM_SIZE)], self.fitness)
+            for _ in range(self.POPULATION_SIZE)]
         self.MUTATION_PROBABILITY = mutation_probability
         self.log = log
+        self.generations = []
 
     def run(self):
-        maxFitness, minFitness, meanFitness, generations = [], [], [], []
+        generations = self.generations
         for generation in range(self.MAX_GENERATIONS):
-            fitness = [self.fitness(*phenotype.genotype) for phenotype in self.population]
+            fitness = [phenotype.fitness() for phenotype in self.population]
 
             if self.log:
-                maxFitness.append(max(fitness))
-                minFitness.append(min(fitness))
-                meanFitness.append(sum(fitness) / len(fitness))
                 generations.append(self.population)
 
             mating_pool = self.__selection__(fitness)
             elite_size = 1 if int(self.POPULATION_SIZE * 0.1) == 0 else int(self.POPULATION_SIZE * 0.1)
             elite = sorted(self.population,
-                           key=lambda phenotype: self.fitness(*phenotype.genotype),
+                           key=lambda phenotype: phenotype.fitness(),
                            reverse=False)[:elite_size]
             next_gen = self.__crossover__(mating_pool=mating_pool, size=self.POPULATION_SIZE - elite_size)
             if self.MUTATION_PROBABILITY > 0:
                 self.__mutate__(next_gen)
             self.population = elite + next_gen
-        if self.log:
-            self.logs = maxFitness, minFitness, meanFitness, generations
-        self.result = min([self.fitness(*phenotype.genotype) for phenotype in self.population])
+        self.result = min([phenotype.fitness() for phenotype in self.population])
 
     def __selection__(self, fitness_values):
         """Proportional selection for minimization"""
@@ -72,8 +70,8 @@ class GenAlgo:
 
     def __single_point_crossover__(self, parent1: Phenotype, parent2: Phenotype):
         point = random.randint(1, self.PARAM_SIZE - 1)
-        child1 = Phenotype(parent1.genotype[:point] + parent2.genotype[point:])
-        child2 = Phenotype(parent2.genotype[:point] + parent1.genotype[point:])
+        child1 = Phenotype(parent1.genotype[:point] + parent2.genotype[point:], self.fitness)
+        child2 = Phenotype(parent2.genotype[:point] + parent1.genotype[point:], self.fitness)
         return [child1, child2]
 
     def __mutate__(self, new_generation: list[Phenotype]):
@@ -82,32 +80,5 @@ class GenAlgo:
             parameter = random.randint(0, self.PARAM_SIZE - 1)
             new_generation[i].genotype[parameter] += random.randint(-1, 1) * random.random()
 
-    def get_logs(self) -> tuple[list, list, list, list]:
-        return self.logs
-
-
-def init_plot(bounds: tuple[float, float]):
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    X = np.arange(bounds[0], bounds[1], 0.5)
-    Y = np.arange(bounds[0], bounds[1], 0.5)
-    X, Y = np.meshgrid(X, Y)
-    Z = X ** 2 + 1.5 * Y ** 2 - 2 * X * Y + 4 * X - 8 * Y
-
-    ax1.set_xlabel('Generation')
-    ax1.set_ylabel('Fitness function')
-    ax1.set_title('max/mean/min fitness function')
-
-
-if __name__ == '__main__':
-    def func(x: float, y: float) -> float:
-        return x ** 2 + 1.5 * y ** 2 - 2 * x * y + 4 * x - 8 * y  # optimum: (2.0;4.0) = -12.0;
-
-
-    algo = GenAlgo(objective_function=func,
-                   bounds=(-100, 100),
-                   population_size=100,
-                   max_generations=200,
-                   mutation_probability=0.1,
-                   log=True)
-    algo.run()
-    maxF, minF, meanFs, generationsList = algo.get_logs()
+    def get_logs(self) -> list:
+        return self.generations
