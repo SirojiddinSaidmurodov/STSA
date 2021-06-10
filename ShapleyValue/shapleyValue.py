c08947622325d2ca24455e5d20fde4797250e42a
SirojiddinSaidmurodov <siroj.serj15@outlook.com>
2021-06-10 15:31:30 +0300

    refactor: defined function
    Took 22 minutes

--- ShapleyValue/shapleyValue.py
+++ ShapleyValue/shapleyValue.py
@@ -1,18 +1,21 @@
 import json
-from math import factorial
+from math import factorial as fc
+
+
+def get_cost_sharing(n, coalitions):
+    result = {}
+    for player in range(1, n + 1):
+        w = 0
+        for coop in coalitions.keys():
+            if str(player) in coop:
+                k = len(coop)
+                w += fc(n - k) * fc(k - 1) * (coalitions.get(coop) - coalitions.get(coop.replace(str(player), "")))
+        result[player] = w / fc(n)
+    return result
+
 
 with open("coalitions.json", "r") as readFile:
     coalitions_data: dict = json.load(readFile)
-n = coalitions_data.get("n")
-coalitions: dict = coalitions_data.get("coalitions")
-print(coalitions)
-result = []
-for player in range(1, n + 1):
-    w = 0
-    for coalition in coalitions.keys():
-        if str(player) in coalition:
-            k = len(coalition)
-            w += factorial(n - k) * factorial(k - 1) * (
-                    coalitions.get(coalition) - coalitions.get(coalition.replace(str(player), "")))
-    result.append(w / factorial(n))
-print(result)
+players_number, coalitions_from_json = coalitions_data.get("n"), coalitions_data.get("coalitions")
+print(coalitions_from_json)
+print(get_cost_sharing(players_number, coalitions_from_json))
